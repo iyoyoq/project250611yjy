94d0919fa2ee1be402662e2828a43d66fcc5dfba
iyoyoq <static2000@163.com>
2025-06-11 23:48:14 +0800

    init: 创建出三个微服务项目

---
+++ permission-service/src/test/java/cn/beforedark/permissionservice/PermissionServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.beforedark.permissionservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PermissionServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
